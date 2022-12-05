bff32aac57a87febde56fe5b49319c6ddb2584a9
Muditha Madusith <kabmmadusith2003@gmail.com>
2022-12-05 12:46:32 +0530

    First commit

---
+++ module.js
@@ -0,0 +1,11 @@
+var myobj={};
+
+myobj.func1=function func1() {
+    console.log("Mamai Pakayaa")
+}
+
+myobj.func2=function func2() {
+    console.log("Mamai Pakayaa2")
+}
+
+module.exports=myobj;
