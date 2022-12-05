5b0e303f5c976043e639f70464609351d4458934
Muditha Madusith <kabmmadusith2003@gmail.com>
2022-12-05 12:54:48 +0530

    add tute3.js

---
+++ tute3.js
@@ -0,0 +1,10 @@
+var mod = require('./module');
+var http = require('http');
+
+http.createServer(function(req,res){
+    res.end("Hello World");   
+}).listen(8080);
+console.log("nodejs server is listning")
+
+mod.func1();
+mod.func2();
